fd0c8328eda4a8005ea5e91458aa169a501d6192
curry <VanishingDante@gmail.com>
2021-07-15 17:22:15 +0800

    update

--- user_lib/src/lib.rs
+++ user_lib/src/lib.rs
@@ -9,11 +9,8 @@ extern crate lazy_static;
 #[macro_use]
 pub mod console;
 pub mod lang_items;
-pub mod memory;
 pub mod prelude;
 mod syscall;
-
-use memory::layout::{BSS_END, BSS_START};
 
 pub use syscall::*;
 
@@ -22,10 +19,6 @@ pub const STDOUT: usize = 1;
 #[no_mangle]
 #[link_section = ".text.entry"]
 pub extern "C" fn _start() -> ! {
-    (*BSS_START..*BSS_END).for_each(|addr| unsafe {
-        (addr as *mut u8).write_volatile(0);
-    });
-
     exit(main());
 
     panic!("unreachable after sys_exit!");
